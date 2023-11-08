23a67612ca00ac20f122b3fb41e3c738e1e37198
Iranzi Dev <iranzithierry12@gmail.com>
2023-11-08 23:42:54 +0200

    Update ApplicationLogo.jsx

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -1,5 +1,5 @@
 export default function ApplicationLogo(props) {
     return (
-      <img {...props} src='/images/logo.png' alt='Youth Code Camp Logo' className="h-24 w-24"/>
+      <img {...props} src='/images/logo.png' alt='Youth Code Camp Logo' className="h-52 w-52"/>
     );
 }
